65cdcaee45e0fc0e0ea66bce6002ca50a3f262d8
siede2010 <63005632+siede2010@users.noreply.github.com>
2022-11-19 20:14:41 +0100

    okeeeee

--- src/exhiber/world/block/distribution/CarPoint.java
+++ src/exhiber/world/block/distribution/CarPoint.java
@@ -4,32 +4,23 @@ import arc.Core;
 import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Lines;
-import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
-import arc.math.geom.Geometry;
-import arc.math.geom.Point2;
 import arc.math.geom.Vec2;
 import arc.util.Time;
 import arc.util.Tmp;
-import mindustry.ai.UnitCommand;
+import arc.util.io.Reads;
+import arc.util.io.Writes;
 import mindustry.core.Renderer;
 import mindustry.entities.TargetPriority;
 import mindustry.gen.Building;
 import mindustry.gen.Unit;
 import mindustry.graphics.Drawf;
 import mindustry.graphics.Layer;
-import mindustry.graphics.Pal;
 import mindustry.type.Item;
 import mindustry.world.Block;
 import mindustry.world.Tile;
-import mindustry.world.blocks.distribution.ItemBridge;
 
-import java.util.List;
-import java.util.Stack;
-
-
-import static mindustry.Vars.tilesize;
-import static mindustry.Vars.world;
+import static mindustry.Vars.*;
 
 public class CarPoint extends Block {
     public float range = 10;
@@ -48,9 +39,16 @@ public class CarPoint extends Block {
         priority = TargetPriority.transport;
         sync = true;
         drawDisabled = false;
-
-        config(Point2.class, (CarPoint.CarPointBuild tile, Point2 i) -> tile.link = Point2.pack(i.x + tile.tileX(), i.y + tile.tileY()));
-        config(Integer.class, (CarPoint.CarPointBuild tile, Integer i) -> tile.link = i);
+        config(Integer.class, (CarPoint.CarPointBuild tile, Integer i) -> {
+            if (i != -1) {
+                tile.targetPos = world.tile(i).build;
+                tile.link = i;
+            }
+            else {
+                tile.link = -1;
+                tile.targetPos = null;
+            }
+        });
     }
     @Override
     public void drawPlace(int x, int y, int rotation, boolean valid){
@@ -60,17 +58,10 @@ public class CarPoint extends Block {
     public class CarPointBuild extends Building
     {
         float r = 0;
-        public List<Block> Connections;
         public boolean move = false;
         public int link = -1;
         public Unit unit;
         public Building targetPos;
-
-        @Override
-        public void created()
-        {
-            Connections = new Stack<>();
-        }
 
         @Override
         public void updateTile(){
@@ -78,7 +69,8 @@ public class CarPoint extends Block {
                 unit = null;
             }
             if (targetPos != null && unit != null && link != -1) {
-
+                if (!unit.isCommandable())
+                    unit.health--;
                 if (Mathf.sqrt(Mathf.pow(unit.getX() - tile.getX(),2) + Mathf.pow(unit.getY() - tile.getY(),2)) < 2) {
                     if (!unit.hasItem()) {
                         if (this.items != null &&this.items.total() != 0) {
@@ -103,13 +95,9 @@ public class CarPoint extends Block {
                             unit.command().commandPosition(new Vec2(targetPos.x, targetPos.y));
                         else
                             unit.command().commandPosition(new Vec2(this.x,this.y));
-                    else
-                        unit.health--;
                 else
                     if (unit.isCommandable())
                         unit.command().commandPosition(new Vec2(this.x,this.y));
-                    else
-                        unit.health--;
                 if (Mathf.sqrt(Mathf.pow(unit.getX() - targetPos.getX(),2) + Mathf.pow(unit.getY() - targetPos.getY(),2)) < 2) {
                     if (((CarPointBuild)targetPos).unit == null) {
                         ((CarPointBuild) targetPos).unit = unit;
@@ -123,15 +111,12 @@ public class CarPoint extends Block {
         }
 
         public boolean linkValid(Tile tile, Tile other) {
-            if(other == null || other.build == null) {configure(-1); return false;}
+            if(other == null || other.build == null) return false;
             if(tile == other || tile == null ) return false;
 
             if(tile.build.team != other.build.team) return false;
             float distance = Mathf.sqrt(Mathf.pow((other.x - tile.x),2f) + Mathf.pow((other.y - tile.y),2));
-            if((other.block() instanceof CarPoint c && ((CarPoint)other.block()).railId == ((CarPoint)tile.block()).railId && distance < range)){
-                return true;
-            }
-            return false;
+            return other.block() instanceof CarPoint c && c.railId == ((CarPoint) tile.block()).railId && distance < range;
         }
 
         @Override
@@ -144,7 +129,6 @@ public class CarPoint extends Block {
         public boolean onConfigureBuildTapped(Building other){
             if(other instanceof CarPointBuild b && b.link == pos()){
                 configure(other.pos());
-                targetPos = other;
                 other.configure(-1);
                 return true;
             }
@@ -153,7 +137,6 @@ public class CarPoint extends Block {
                 if(link == other.pos()){
                     configure(-1);
                 }else{
-                    targetPos = other;
                     configure(other.pos());
                 }
                 return false;
@@ -163,9 +146,7 @@ public class CarPoint extends Block {
 
         @Override
         public boolean acceptItem(Building source, Item item){
-            if(hasItems && items.total() < itemCapacity)
-                return true;
-            return false;
+            return hasItems && items.total() < itemCapacity;
         }
 
         @Override
@@ -176,17 +157,16 @@ public class CarPoint extends Block {
             Draw.rect(Core.atlas.find(name),x,y,0);
 
             Draw.z(Layer.power);
-
+            if (link == -1) return;
             Tile other = (world.tile(link));
             if(!linkValid(tile, other) && !(link == -1)) return;
 
             if(Mathf.zero(Renderer.bridgeOpacity)) return;
 
             if (linkValid(tile, other) && other != null && other.block() != null) {
-                int i = relativeTo(other.x, other.y);
                 Draw.color(Color.white, Color.black, Mathf.absin(Time.time, 6f, 0.07f));
 
-                Draw.alpha((1f) * Renderer.bridgeOpacity);
+                Draw.alpha(Renderer.bridgeOpacity);
 
                 Draw.rect(Core.atlas.find(name+"-spin"), x, y, r);
                 Draw.rect(Core.atlas.find(name+"-spin"), other.drawx(), other.drawy(), r);
@@ -208,9 +188,6 @@ public class CarPoint extends Block {
                         mx + Tmp.v1.x*2*railSize,
                         my + Tmp.v1.y*2*railSize,
                         false);
-
-
-                int dist = Math.max(Math.abs(other.x - tile.x), Math.abs(other.y - tile.y)) - 1;
             }
 
             Draw.color();
@@ -225,8 +202,8 @@ public class CarPoint extends Block {
         }
 
         @Override
-        public Point2 config(){
-            return Point2.unpack(link).sub(tile.x, tile.y);
+        public Object config(){
+            return link;
         }
 
         @Override
@@ -242,6 +219,26 @@ public class CarPoint extends Block {
             if (unit != null)
                 unit.kill();
             super.killed();
+        }
+
+        @Override
+        public void write(Writes write){
+            super.write(write);
+            write.i(link);
+        }
+
+        @Override
+        public void read(Reads read, byte revision){
+            super.read(read, revision);
+            int temp = read.i();
+            if (temp != -1) {
+                targetPos = world.tile(temp).build;
+                if (targetPos != null && !linkValid(this.tile, targetPos.tile))
+                    temp = -1;
+            }
+            else
+                targetPos = null;
+            link = temp;
         }
     }
 }
